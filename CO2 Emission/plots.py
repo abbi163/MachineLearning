5595be7e78aee1c63d40b5368b7cfaebeb4b7edd
Abhijeet Anand <34609032+abbi163@users.noreply.github.com>
2018-10-17 21:32:58 +0200

    Add files via upload
    This is just the codes of the plots (scatter plot, histogram)

---
+++ CO2 Emission/plots.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("E:/Pythoncode/Coursera/CO2 Emission/FuelConsumption.csv")
+#print(df.describe())
+
+# choosing data only on these following four rows into new dataframe "cdf"
+# hist -> histogram
+cdf = df[["ENGINESIZE","CYLINDERS","FUELCONSUMPTION_COMB","CO2EMISSIONS"]]
+cdf.hist()
+plt.show()
+
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS, color = 'blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("CO2Emission")
+plt.show()
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS, color = 'blue')
+plt.xlabel("ENGINESIZE")
+plt.ylabel("CO2Emission")
+plt.show()
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color = 'blue')
+plt.xlabel("CYLINDERS")
+plt.ylabel("CO2Emission")
+plt.show()
